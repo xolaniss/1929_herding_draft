d760053e423c2c59b7069f3e500e4fa6ea88f0c1
Xolani Sibande <xolaniss@gmail.com>
2024-01-11 17:35:17 +0200

    probit marginal effects estimations

--- Scripts/11_dummy_on_dummy.R
+++ Scripts/11_dummy_on_dummy.R
@@ -43,22 +43,43 @@ options(scipen = 999)
 # Functions ---------------------------------------------------------------
 source(here("Functions", "fx_plot.R"))
 source(here("Functions", "group_dummy_dummy.R"))
-marginal_effect <- 
+probit_marginal_effect <- 
   function(data, category) {
-    glm(formula, 
-        family = binomial(link = "probit"),
-        data =  data %>%  filter(Category == category)) %>% 
-      margins::margins(data = data)
+   models <-  probitmfx(formula, 
+              data =  data %>%  filter(Category == category),
+              robust = TRUE,
+              atmean = FALSE) 
+    coefs <- models$mfxest[, 1] %>% bind_rows()
+    pvalues <- models$mfxest[, 4] %>% bind_rows()
+    bind_cols(coefs, pvalues) 
   }
 
-industry <- list("All industries",
-                 "Business services",
-                 "Consumables",
-                 "Health",
-                 "Manufacturing",
-                 "Other"
-                 
-)
+group_probit_marginal_effect <- 
+  function(data, group_vec = industry, group_type = "General Herding") {
+    group_vec %>% 
+      map(~probit_marginal_effect(data = data,
+                                  .x)) %>% 
+      bind_rows() %>% 
+      mutate(
+        stars_party = ifelse(party_dummy...3 < 0.001, "***",
+                             ifelse(party_dummy...3  < 0.01, "**", 
+                                    ifelse(party_dummy...3  < 0.05, "*", "")))
+      ) %>% 
+      mutate(
+        stars_volatility = ifelse(volatility...4 < 0.001, "***",
+                                  ifelse(volatility...4  < 0.01, "**", 
+                                         ifelse(volatility...4  < 0.05, "*", "")))
+      ) %>% 
+      mutate(across(1:2, ~strtrim(., 6))) %>% 
+      mutate(Party = paste0(party_dummy...1, stars_party)) %>% 
+      mutate(Volatility = paste0(volatility...2, stars_volatility)) %>% 
+      dplyr::select(Party, Volatility) %>% 
+      mutate(
+        Category = group_vec %>% transpose() %>% unlist(),
+        Group = group_type
+      ) %>% 
+      relocate(Category, .before = Party)
+  }
 
 # Importing -------------------------------------------------------------
 presidential <- read_rds(here("Outputs", "artifacts_presidential_terms.rds"))
@@ -85,6 +106,15 @@ volatility_tbl <-
   )
 
 volatility_tbl
+
+industry <- list("All industries",
+                 "Business services",
+                 "Consumables",
+                 "Health",
+                 "Manufacturing",
+                 "Other"
+                 
+)
 
 # General herding estimation ----------------------------------------------
 # selecting date and herding (a2)
@@ -170,30 +200,46 @@ ols_party_dummy_model_tbl
 formula <-  as.formula(a2_dummy ~ party_dummy + volatility)
 
 #  marginal effects for "All industries", "Consumables", "Health", "Manufacturing", "Other", "Business services" 
-
-industry %>% 
-  map(~marginal_effect(data = general_herding_a2_presidential_tbl %>% 
-                         left_join(volatility_tbl, by = c("Date" = "Date")) ,
-                       .x))
-
+general_herding_tbl <- 
+  general_herding_a2_presidential_tbl %>% 
+  left_join(volatility_tbl, by = c("Date" = "Date"))       
+   
+general_herding_probit <- group_probit_marginal_effect(data = general_herding_tbl)
+general_herding_probit
+  
 # Probit - Fundamental herding  ---------------------------------------------------------
 formula <- as.formula(a2_fund_dummy ~ party_dummy + volatility)
-industry %>% 
-  map(~marginal_effect(data = fundamental_herding_a2_presidential_tbl %>% 
-                         left_join(volatility_tbl, by = c("Date" = "Date")), 
-                       .x))
+fundamental_herding_tbl <- 
+  fundamental_herding_a2_presidential_tbl %>% 
+  left_join(volatility_tbl, by = c("Date" = "Date"))
 
+fundamental_herding_probit <- group_probit_marginal_effect(data = fundamental_herding_tbl,
+                                                           group_type = "Fundamental Herding")
+fundamental_herding_probit
 
 # Non-Fundamental herding - Probit ---------------------------------------------------------
 formula <- as.formula(a2_nonfund_dummy ~ party_dummy + volatility)
-industry %>% 
-  map(~marginal_effect(data = non_fundamental_herding_a2_presidential_tbl %>% 
-                         left_join(volatility_tbl, by = c("Date" = "Date")), 
-                       .x))
+nonfundamental_herding_tbl <- 
+  non_fundamental_herding_a2_presidential_tbl %>% 
+  left_join(volatility_tbl, by = c("Date" = "Date"))
+
+nonfundamental_herding_probit <- group_probit_marginal_effect(data = nonfundamental_herding_tbl, 
+                                                              group_type = "Non-Fundamental Herding")
+nonfundamental_herding_probit
+
+# Combined - Probit ----------------------------------------------------------------
+# combine all the models
+probit_party_dummy_model_tbl <- 
+  bind_rows(
+    general_herding_probit,
+    fundamental_herding_probit,
+    nonfundamental_herding_probit
+  )
 
 # Export ---------------------------------------------------------------
 artifacts_party_dummy <- list (
-  ols_party_dummy_model_tbl = ols_party_dummy_model_tbl
+  ols_party_dummy_model_tbl = ols_party_dummy_model_tbl,
+  probit_party_dummy_model_tbl = probit_party_dummy_model_tbl
 )
 
 write_rds(artifacts_party_dummy, file = here("Outputs", "artifacts_party_dummy.rds"))
